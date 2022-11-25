94e5e53a4587f3b4be4091185a440d14697a4b79
JuanCamiloMB <63174756+JuanCamiloMB@users.noreply.github.com>
2022-11-25 16:32:33 -0500

    Updates

---
+++ precache-manifest.a3fdacb289445058a0c8a0bfcaaccd4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7be75e57404abaa82b89",
+    "url": "JuanCamiloMB.github.io/mypage/static/css/main.80563d8d.chunk.css"
+  },
+  {
+    "revision": "7be75e57404abaa82b89",
+    "url": "JuanCamiloMB.github.io/mypage/static/js/main.7be75e57.chunk.js"
+  },
+  {
+    "revision": "7676222ee57d26ff7f32",
+    "url": "JuanCamiloMB.github.io/mypage/static/js/1.7676222e.chunk.js"
+  },
+  {
+    "revision": "8a073a161ba12291daf8",
+    "url": "JuanCamiloMB.github.io/mypage/static/js/runtime~main.8a073a16.js"
+  },
+  {
+    "revision": "326ff01f262720b6896dfea65fc4103d",
+    "url": "JuanCamiloMB.github.io/mypage/static/media/certificate.326ff01f.png"
+  },
+  {
+    "revision": "4c7a3b44ec2f87c822acd92ce4912446",
+    "url": "JuanCamiloMB.github.io/mypage/index.html"
+  }
+];
