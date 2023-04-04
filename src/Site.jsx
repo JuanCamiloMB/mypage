d257c6ffa265ca34988b962841dce93a76d82a15
JuanK <juancamilo.manjarres@gmail.com>
2023-04-04 16:31:15 -0500

    Router fix

--- src/Site.jsx
+++ src/Site.jsx
@@ -1,17 +1,17 @@
 import './Site.css'
 import ProjectPage from './pages/ProjectsPage'
 import AboutPage from './pages/AboutPage'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 
 function Site() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<ProjectPage/>}/>
         <Route path='/about' element={<AboutPage/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
