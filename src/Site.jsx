14a4f82a3449ea7d54c8d03983af5c2f7ce49827
JuanK <juancamilo.manjarres@gmail.com>
2023-04-25 11:31:07 -0500

    blob

--- src/Site.jsx
+++ src/Site.jsx
@@ -4,21 +4,42 @@ import AboutPage from "./pages/AboutPage";
 import { Routes, Route, useLocation } from "react-router-dom";
 import PhD from "./components/Projects/PhD";
 import { AnimatePresence } from "framer-motion";
-import Empty from "./components/Empty"
+import Empty from "./components/Empty";
+import { useEffect } from "react";
 
 function Site() {
   const location = useLocation();
+
+  useEffect(() => {
+    const blob = document.getElementById("blob");
+    document.body.onpointermove = (event) => {
+      /* console.log(event) */
+
+      const { clientX, clientY } = event;
+
+      blob.animate(
+        {
+          left: clientX + "px",
+          top: clientY + "px",
+        },
+        { duration: 3000, fill: "forwards" }
+      );
+    };
+  });
+
   return (
-    
+    <>
+      <div id="blob"></div>
+      <div id="blur"></div>
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<ProjectPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/PhD" element={<PhD />} />
-          <Route path="*" element={<Empty/>}/>
+          <Route path="*" element={<Empty />} />
         </Routes>
       </AnimatePresence>
-    
+    </>
   );
 }
 
